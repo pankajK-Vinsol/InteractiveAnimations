7439de64b2ff81304d14d27b8c6f1fba67f0bb4c
Pankaj Kumar <pankaj.kumar@vinsol.com>
2019-05-14 19:11:17 +0530

    commit updated with blur effect and transition animation.

---
+++ InteractiveAnimations/InteractiveViewController.swift
@@ -0,0 +1,242 @@
+//
+//  InteractiveViewController.swift
+//  InteractiveAnimations
+//
+//  Created by Pankaj kumar on 14/05/19.
+//  Copyright © 2019 Pankaj kumar. All rights reserved.
+//
+
+import UIKit
+import UIKit.UIGestureRecognizerSubclass
+
+private enum State {
+    case closed
+    case open
+}
+
+extension State {
+    var opposite: State {
+        switch self {
+        case .open:
+            return .closed
+        case .closed:
+            return .open
+        }
+    }
+}
+
+class InteractiveViewController: UIViewController {
+
+    @IBOutlet weak private var contentImageView: UIImageView!
+    
+    private var currentState: State = .closed
+    private var runningAnimators = [UIViewPropertyAnimator]()
+    private var animationProgress = [CGFloat]()
+    private let popupOffset: CGFloat = 440.0
+    private var bottomConstraint = NSLayoutConstraint()
+    
+    private lazy var popupView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .white
+        view.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.1
+        view.layer.shadowRadius = 10
+        return view
+    }()
+    
+    private lazy var closedTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Comments"
+        label.font = UIFont.systemFont(ofSize: 14, weight: UIFont.Weight.medium)
+        label.textColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var openTitleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Comments"
+        label.font = UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.heavy)
+        label.textColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        label.textAlignment = .center
+        label.alpha =  0
+        label.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        layout()
+        popupView.addGestureRecognizer(panRecognizer)
+    }
+
+    private func layout() {
+        
+        popupView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(popupView)
+        popupView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        popupView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        bottomConstraint = popupView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: popupOffset)
+        bottomConstraint.isActive = true
+        popupView.heightAnchor.constraint(equalToConstant: 500).isActive = true
+        
+        closedTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        popupView.addSubview(closedTitleLabel)
+        closedTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        closedTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        closedTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 20).isActive = true
+        
+        openTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+        popupView.addSubview(openTitleLabel)
+        openTitleLabel.leadingAnchor.constraint(equalTo: popupView.leadingAnchor).isActive = true
+        openTitleLabel.trailingAnchor.constraint(equalTo: popupView.trailingAnchor).isActive = true
+        openTitleLabel.topAnchor.constraint(equalTo: popupView.topAnchor, constant: 30).isActive = true
+    }
+    
+    private lazy var panRecognizer: InstantPanGestureRecognizer = {
+        let recognizer = InstantPanGestureRecognizer()
+        recognizer.addTarget(self, action: #selector(popupViewPanned(recognizer:)))
+        return recognizer
+    }()
+    
+    private func animateTransitionIfNeeded(to state: State, duration: TimeInterval) {
+        
+        guard runningAnimators.isEmpty else { return }
+        let transitionAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1, animations: {
+            switch state {
+            case .open:
+                self.bottomConstraint.constant = 0
+                self.popupView.layer.cornerRadius = 20
+                self.closedTitleLabel.transform = CGAffineTransform(scaleX: 1.6, y: 1.6).concatenating(CGAffineTransform(translationX: 0, y: 15))
+                self.openTitleLabel.transform = .identity
+                self.contentImageView.addBlurEffect()
+                
+            case .closed:
+                self.bottomConstraint.constant = self.popupOffset
+                self.popupView.layer.cornerRadius = 0
+                self.closedTitleLabel.transform = .identity
+                self.openTitleLabel.transform = CGAffineTransform(scaleX: 0.65, y: 0.65).concatenating(CGAffineTransform(translationX: 0, y: -15))
+                self.contentImageView.removeBlurEffect()
+            }
+            self.view.layoutIfNeeded()
+        })
+        
+        transitionAnimator.addCompletion { position in
+            switch position {
+            case .start:
+                self.currentState = state.opposite
+            case .end:
+                self.currentState = state
+            case .current:
+                ()
+            @unknown default:
+                break
+            }
+            
+            switch self.currentState {
+            case .open:
+                self.bottomConstraint.constant = 0
+            case .closed:
+                self.bottomConstraint.constant = self.popupOffset
+            }
+            self.runningAnimators.removeAll()
+            
+        }
+        
+        let inTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeIn, animations: {
+            switch state {
+            case .open:
+                self.openTitleLabel.alpha = 1
+            case .closed:
+                self.closedTitleLabel.alpha = 1
+            }
+        })
+        inTitleAnimator.scrubsLinearly = false
+        
+        let outTitleAnimator = UIViewPropertyAnimator(duration: duration, curve: .easeOut, animations: {
+            switch state {
+            case .open:
+                self.closedTitleLabel.alpha = 0
+            case .closed:
+                self.openTitleLabel.alpha = 0
+            }
+        })
+        outTitleAnimator.scrubsLinearly = false
+        
+        transitionAnimator.startAnimation()
+        inTitleAnimator.startAnimation()
+        outTitleAnimator.startAnimation()
+        
+        runningAnimators.append(transitionAnimator)
+        runningAnimators.append(inTitleAnimator)
+        runningAnimators.append(outTitleAnimator)
+        
+    }
+    
+    @objc private func popupViewPanned(recognizer: UIPanGestureRecognizer) {
+        switch recognizer.state {
+        case .began:
+            animateTransitionIfNeeded(to: currentState.opposite, duration: 1)
+            runningAnimators.forEach { $0.pauseAnimation() }
+            animationProgress = runningAnimators.map { $0.fractionComplete }
+            
+        case .changed:
+            let translation = recognizer.translation(in: popupView)
+            var fraction = -translation.y / popupOffset
+            if currentState == .open { fraction *= -1 }
+            if runningAnimators[0].isReversed { fraction *= -1 }
+            for (index, animator) in runningAnimators.enumerated() {
+                animator.fractionComplete = fraction + animationProgress[index]
+            }
+            
+        case .ended:
+            let yVelocity = recognizer.velocity(in: popupView).y
+            let shouldClose = yVelocity > 0
+            if yVelocity == 0 {
+                runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
+                break
+            }
+            
+            // reverse the animations based on their current state and pan motion
+            switch currentState {
+            case .open:
+                if !shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+                if shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+            case .closed:
+                if shouldClose && !runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+                if !shouldClose && runningAnimators[0].isReversed { runningAnimators.forEach { $0.isReversed = !$0.isReversed } }
+            }
+            runningAnimators.forEach { $0.continueAnimation(withTimingParameters: nil, durationFactor: 0) }
+            
+        default:
+            ()
+        }
+    }
+    
+}
+
+/// A pan gesture that enters into the `began` state on touch down instead of waiting for a touches moved event.
+class InstantPanGestureRecognizer: UIPanGestureRecognizer {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent) {
+        if (self.state == UIGestureRecognizer.State.began) { return }
+        super.touchesBegan(touches, with: event)
+        self.state = UIGestureRecognizer.State.began
+    }
+}
+
+extension UIImageView {
+    func addBlurEffect() {
+        let effectView = UIVisualEffectView()
+        effectView.frame = self.frame
+        self.addSubview(effectView)
+        effectView.effect = UIBlurEffect(style: .dark)
+    }
+    
+    func removeBlurEffect() {
+        let blurredEffectViews = self.subviews.filter{$0 is UIVisualEffectView}
+        blurredEffectViews.forEach { blurView in
+            blurView.removeFromSuperview()
+        }
+    }
+}
